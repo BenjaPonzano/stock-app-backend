973fcd25f7c2893ea94a6f167aa5dbf95af0bb72
BenjaPonzano <benjaponzano10@gmail.com>
2026-06-16 19:57:35 -0300

    feat: nombre de usuario en respuesta del login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,7 +29,7 @@ router.post('/login', async (req, res) => {
       { expiresIn: '8h' }
     )
 
-    res.json({ token, tipoUsuario: usuario.tipoUsuario })
+    res.json({ token, tipoUsuario: usuario.tipoUsuario, nombre: usuario.nombre })
 
   } catch (error) {
     res.status(500).json({ mensaje: 'Error al iniciar sesión', error: error.message })
--- src/services/usuario.service.js
+++ src/services/usuario.service.js
@@ -1,17 +1,30 @@
 const Usuario = require('../models/Usuario')
+const bcrypt = require('bcryptjs')
 
-const getAll = async () => Usuario.findAll()
+const getAll = async () => Usuario.findAll({
+  attributes: { exclude: ['password'] }
+})
 
 const getById = async (id) => {
-  const usuario = await Usuario.findByPk(id)
+  const usuario = await Usuario.findByPk(id, {
+    attributes: { exclude: ['password'] }
+  })
   return usuario || null
 }
 
-const create = async (data) => Usuario.create(data)
+const create = async (data) => {
+  if (data.password) {
+    data.password = await bcrypt.hash(data.password, 10)
+  }
+  return Usuario.create(data)
+}
 
 const update = async (id, data) => {
   const usuario = await Usuario.findByPk(id)
   if (!usuario) return null
+  if (data.password) {
+    data.password = await bcrypt.hash(data.password, 10)
+  }
   await usuario.update(data)
   return usuario
 }
